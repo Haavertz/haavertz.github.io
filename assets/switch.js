b1d50638a247870922b307578177147441b0127a
Gleison <gleisonmorais06@gmail.com>
2025-05-06 22:25:05 -1200

    Update: Resume.html

--- assets/switch.js
+++ assets/switch.js
@@ -1,6 +1,6 @@
 function switchVisible() {
     if (document.getElementById('box')) {
-            if (document.getElementById('box').style.display == 'none') {
+        if (document.getElementById('box').style.display == 'none') {
             document.getElementById('box').style.display = 'block';
             document.getElementById('dirbox').style.display = 'none';
             document.title = 'Haavertz';
