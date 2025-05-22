c1e0ca95a9a0bf66a1b1a02f9887f17c347ea8d9
Gleison <gleisonmorais06@gmail.com>
2025-05-22 04:10:24 -1200

    Feat: resume path for index

--- portfolio/as/v1/js/haavertz.js
+++ portfolio/as/v1/js/haavertz.js
@@ -1,23 +1,16 @@
 (async function () {
   const nav_buttons = document.querySelectorAll('.button');
-  nav_buttons.forEach(nav_buttons => {
-    nav_buttons.addEventListener('click', () => {
+  const map_path = { 0 : "#", 1 : "#", 2 : "#", 3 : "#", 4 : "./resume.html", 5 : "#" }
+  nav_buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
       document.querySelector('.button.active')?.classList.remove('active');
-      nav_buttons.classList.add('active');
+      button.classList.add('active');
+      
+      const path = map_path[index];
+      if (path) { window.location.href = path; }
       
     });
   });
-  // function updateClock() {
-  //   const now = new Date();
-  //   let hours = now.getHours();
-  //   const minutes = now.getMinutes().toString().padStart(2, '0');
-  //   const ampm = hours >= 12 ? 'PM' : 'AM';
-  //   hours = hours % 12 || 12; 
-  //   document.getElementById('clock').textContent = `${hours}:${minutes} ${ampm}`;
-  // }
-
-  // updateClock()
-
   async function download(language) {
     if (!["pt", "en"].includes(language))
       language = "en";
